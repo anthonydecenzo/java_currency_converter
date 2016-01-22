4b67bcea042fc878301f1ff48c2d6b5ff57a67ae
Anthony DeCenzo <anthonydecenzo8@gmail.com>
2016-01-21 22:49:40 -0500

    Finished the USD to CAD conversion.

--- src/java_currency_converter/execute.java
+++ src/java_currency_converter/execute.java
@@ -1,26 +1,50 @@
 package java_currency_converter;
 
-import javax.swing.JFrame;
-import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
-import javax.swing.JComboBox;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
+import javax.swing.*;
 
-public class execute {
-	JButton convertButton = new JButton("Convert");
-	JLabel label1 = new JLabel("Convert         to          this");
-	JComboBox<String> originalCurrency = new JComboBox<String>();
-	JComboBox<String> convertedCurrency = new JComboBox<String>();
-	JFrame frame = new JFrame();
-	JTextField originalCurrencyTextField = new JTextField();
-	JTextField convertedCurrencyTextField = new JTextField();
-	JLabel equalsLabel = new JLabel("is = to");
-	public static void main(String [] args)
+import org.apache.commons.io.IOUtils;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.net.*;
+
+public class execute implements ActionListener {
+	public JButton convertButton = new JButton("Convert");
+	public JLabel label1 = new JLabel("Convert         to          this");
+	public JComboBox<String> originalCurrency = new JComboBox<String>();
+	public JComboBox<String> convertedCurrency = new JComboBox<String>();
+	public JFrame frame = new JFrame();
+	public JTextField originalCurrencyTextField = new JTextField();
+	public JTextField convertedCurrencyTextField = new JTextField();
+	public JLabel equalsLabel = new JLabel("is = to");
+	public static execute program = new execute();
+	public static void main(String [] args) throws MalformedURLException, IOException
 	{
-		execute program = new execute();
+		
 		program.createGUI();
+		
+	}
+	
+	public double getUSDCAD() throws MalformedURLException, IOException
+	{
+
+			URL convertAPI = new URL("http://apilayer.net/api/live?access_key=fb581cf502c6264c783c838c9034b988");
+			String jsonText = IOUtils.toString(convertAPI);
+			JsonParser parser = new JsonParser();
+			JsonObject json = parser.parse(jsonText).getAsJsonObject();
+			
+			String testText = json.get("quotes").toString();
+			json = parser.parse(testText).getAsJsonObject();
+
+			double sum = json.get("USDCAD").getAsDouble();
+			int textField = Integer.parseInt(originalCurrencyTextField.getText());
+			
+			return sum * textField;
 	}
 	
 	
@@ -59,14 +83,21 @@ public class execute {
 		frame.setVisible(true);
 		
 		
-		convertButton.addActionListener(new convertListener());
+		convertButton.addActionListener(this);
+		
 	}
-	
-	class convertListener implements ActionListener
-	{
-		public void actionPerformed(ActionEvent e)
-		{
-			originalCurrencyTextField.setText("test");
+
+	public void actionPerformed(ActionEvent arg0) {
+		double test;
+		try {
+			test = program.getUSDCAD();
+			String text = Double.toString(test);
+			
+			convertedCurrencyTextField.setText(text);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		
 	}
 }
